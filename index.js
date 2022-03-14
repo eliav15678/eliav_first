772bec6a3ed059e1e364c150c922f7b58676aa7d
eliav15678 <eliav15678@gmail.com>
2022-03-14 11:58:32 +0200

    14.03.22/1

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+const express = require('express');
+const bodyparser= require('body-parser');
+const app = express();
 
+app.use(bodyparser.urlencoded());
+app.use(bodyparser.json());
 
+const actionsrouts = require('./actions');
+app.use('/api',actionsrouts);
 
+const port = 5000;
+app.listen(port,function(){
+    console.log(`server is working via port ${port}`);
+})
 
