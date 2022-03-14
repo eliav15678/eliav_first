67a83ebb46f97a77e28a86acfc349bb28ced56ed
eliav15678 <eliav15678@gmail.com>
2022-03-14 13:11:14 +0200

    17.03.22/4

--- actions.js
+++ actions.js
@@ -1,9 +1,9 @@
-const express=require('express');
-const router=express.router();
+const express = require('express');
+const router = express.Router();
 
-router.git('/greeting',(req,res)=>{
-    return res.status(5000).json({
-        Message:'welcome to me api'
-    });
-});
+//router.git('/greeting',(req,res)=>{
+  //  return res.status(200).json({
+  //      Message:'welcome to me api'
+  //  });
+//});
 module.exports=router;
--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const app = express();
 app.use(bodyparser.urlencoded());
 app.use(bodyparser.json());
 
-const actionsrouts = require('./actions.js');
-app.use('/api',actionsrouts);
+const actionsRoute = require('./actions');
+app.use('/api',actionsRoute);
 
 const port = 5000;
 app.listen(port,function(){
