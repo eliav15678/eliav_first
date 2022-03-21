24fd7ac34457669721e899571b213f4c39437bff
eliav15678 <eliav15678@gmail.com>
2022-03-21 12:02:25 +0200

    21.03.2022

--- actions.js
+++ actions.js
@@ -1,9 +1,44 @@
 const express = require('express');
 const router = express.Router();
 
-//router.git('/greeting',(req,res)=>{
-  //  return res.status(200).json({
-  //      Message:'welcome to me api'
-  //  });
-//});
+router.get('/greeting',(req,res)=>{
+  return res.status(200).json({
+       Message:'welcome to me api'
+    });
+});
+
+let users=[
+   {
+        name='eliav elmaliah',
+        age=23
+   },
+   {
+        name='hen gabay',
+        age=25
+   },
+   {  
+        name='shalom elmaliah',
+        age=52
+   },
+   { 
+        name='iris elmaliah',
+        age=47
+   }
+];
+
+router.post('/register',(request,response) => {
+  const {email,password} = request.body;
+  users.push({email:email,password:password});
+
+  //filter
+  const data = users.filter(x=>x.age == 23); //return array
+  //find
+  const data2 = users.find(x=>x.name == 'iris'); //return object
+
+  return response.status(200).json({
+  Message: data
+  });
+
+})
+
 module.exports=router;
