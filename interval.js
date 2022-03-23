9c4d95b78c1c443ca457e80f2ec10e4deef3b5e0
eliav15678 <eliav15678@gmail.com>
2022-03-23 10:32:04 +0200

    Create interval.js

---
+++ interval.js
@@ -0,0 +1,27 @@
+// let x= 9;
+// let y =2;
+// const res = x/y<8 ? 'yes' : 'no';
+// console.log(res);
+
+
+// let timer =5;
+// let x = setInterval(() => {
+//     console.log(timer);
+//     timer --;
+//     if (timer<=0)
+//     {
+//       clearInterval(x)
+//     }
+// }, 1000);
+
+
+// let timer =1;
+// let x = setInterval(() => {
+//     console.log(timer);
+//     timer ++;
+//     if (timer>10)
+//     {
+//       console.log('time out!!');
+//       clearInterval(x)
+//     }
+// }, 1000);
